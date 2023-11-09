e489d9689187db693b1b11f5a8090de36ed5d260
colJY <ljy162518@gmail.com>
2023-11-09 21:29:57 +0900

    Feat : FilterFragment UI

---
+++ app/src/main/java/com/lee/matchmate/main/filter/FilterFragment.kt
@@ -0,0 +1,30 @@
+package com.lee.matchmate.main.filter
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.lee.matchmate.R
+import com.lee.matchmate.common.ViewBindingBaseFragment
+import com.lee.matchmate.databinding.FragmentFilterBinding
+import com.lee.matchmate.main.MainViewModel
+
+class FilterFragment : ViewBindingBaseFragment<FragmentFilterBinding>(FragmentFilterBinding::inflate) {
+
+    private val viewModel : MainViewModel by viewModels()
+    companion object {
+        fun newInstance() = FilterFragment()
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tbFilter.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+}
---
+++ app/src/main/java/com/lee/matchmate/main/filter/FilterViewModel.kt
@@ -0,0 +1,7 @@
+package com.lee.matchmate.main.filter
+
+import androidx.lifecycle.ViewModel
+
+class FilterViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
