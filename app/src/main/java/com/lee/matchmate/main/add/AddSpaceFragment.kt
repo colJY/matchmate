d9a8b72cf959a75fda5da61030f45689f3dc5cc6
colJY <ljy162518@gmail.com>
2023-11-13 01:36:17 +0900

    Feat : Dropdown map Navigate, Add SettingFragment

--- app/src/main/java/com/lee/matchmate/main/add/AddSpaceFragment.kt
+++ app/src/main/java/com/lee/matchmate/main/add/AddSpaceFragment.kt
@@ -6,27 +6,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.lee.matchmate.R
+import com.lee.matchmate.common.ViewBindingBaseFragment
+import com.lee.matchmate.databinding.FragmentAddSpaceBinding
+import com.lee.matchmate.main.detail.DetailViewModel
 
-class AddSpaceFragment : Fragment() {
+class AddSpaceFragment : ViewBindingBaseFragment<FragmentAddSpaceBinding>(FragmentAddSpaceBinding::inflate) {
+    private val viewModel: AddSpaceViewModel by viewModels()
 
     companion object {
         fun newInstance() = AddSpaceFragment()
     }
 
-    private lateinit var viewModel: AddSpaceViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_add_space, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AddSpaceViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tbAdd.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 
 }
